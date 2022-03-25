d61422c83bc730e1bd753b9e67469bb996b5f9db
yamz-11 <wiratamajrz@gmail.com>
2022-03-26 01:31:08 +0700

    Add project files.

---
+++ Insert_Data/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Insert_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().InsertData();
+        }
+        public void InsertData()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=LAPTOP-DMUVDQOK\\WIRATAMA;database=TokoBakery;Integrated Security " +
+                    "= TRUE");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("insert into Kasir (Id_kasir,Nama_kasir,Jenis_Kelamin )values('K01','Wira','L')",
+                    con);
+
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Sukses menambahkan data");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Gagal menambahkan data." + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+    } 
+}
